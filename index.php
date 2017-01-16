855f87332a294bc18f88ebb5ec733544d9fdfdbe
Lilia <geertszelia@gmail.com>
2017-01-16 14:00:06 +0100

    Exercice suivi à finir

--- index.php
+++ index.php
@@ -1,4 +1,59 @@
 <?php
+class StrUtils {
+	private $_str = "";
 
+	function __construct($newStr) {
+		$this -> _str = $newStr;
+	}
 
+	public function getStr() {
+		return $this -> _str; 
+	}
+
+	public function bold() {
+		$bold = '<b>'.$this -> _str.'</b>';
+		$this -> _str = $bold;
+	}
+
+	public function italic() {
+		return '<i>'.$this -> _str.'</i>';
+	}
+
+	public function underline() {
+		return '<u>'.$this -> _str.'</u>';
+	}
+
+	public function capitalize() {
+
+	}
+
+	public function uglify() {
+		$this -> bold();
+		$this -> italic();
+		$this -> underline();
+	}
+}
+
+	$myStr = new StrUtils('Hello world');
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>POO</title>
+</head>
+<body>
+	<div>
+		<ul>
+			<li><?= ("Normal: " . $myStr -> getStr()); ?></li>	
+			<li><?= ('Bold: '. $myStr -> bold()); ?></li>
+			<li><?= ('Italic: '. $myStr -> italic()); ?></li>
+			<li><?= ('Underline: '.$myStr -> underline()); ?></li>
+			<li></li>
+			<li><?= ('Uglify: '.$myStr -> uglify()); ?></li>
+		</ul>
+	</div>
+	
+</body>
+</html>
